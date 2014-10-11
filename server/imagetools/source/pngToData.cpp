be1b28d22c36e65d1dbc508bdbf928aaf1bb5c99
IgorSusmelj <igor_7779@hotmail.com>
2014-10-11 10:02:56 -0700

    added computation for pixel offset

--- server/imagetools/source/pngToData.cpp
+++ server/imagetools/source/pngToData.cpp
@@ -1,9 +1,19 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
 #include <iostream>
 
-#define BLACK_WHITE_THRESHOLD 200
+#include <stdio.h>
+#include <cmath>
+
+
+#define BLACK_WHITE_THRESHOLD 240
 #define ROW_VALUE_THRESHOLD 50
+#define INITIAL_LOWEST_ERROR_AMOUNT 10000
+#define ERROR_TOLERANCE 1
+
+
 
 
 using namespace cv;
@@ -163,27 +173,152 @@ for(int i=size.height-1; i>0; i--)
 	
 
 	//Detect pixel size
-	
-	size = cropped.size();
-		
-/*
+/*	int Match = 0;
+	int counter = 1;
+
+	Mat result;
+
+//while(!Match)
+//{	
+	Mat pattern = cvCreateImage(cvSize(3*counter,3*counter),8,3);
+	size = pattern.size();
 	for(int i=0; i<size.width; i++)
 	{
 		for(int u=0; u<size.height; u++)
 		{
-			
+			if(i>=size.width/3.&&i<size.width*2./3.){
+				if(u>=size.height/3.&&u<size.height*2./3.)
+				{
+					pattern.at<Vec3b>(Point(i,u)) = Vec3b(255,255,255);
+				}
+			}else{
+				
+				pattern.at<Vec3b>(Point(i,u))=Vec3b(0,0,0);
+			}
 		}
-	}*/
+	}
+*/	
+	//Pattern matching	
+	
+	size = cropped.size();
+	
+	int lowestError=10000;
+	int lowestErrorOffset=0;
 
 
-	Mat pattern = cvCreateImage(cvSize(3,3),8,3);	
+	for(int u=0;u<10;u++){
+
+		int amount_of_distances=0;	
+		int lastDistance=0;	
+		int distance=0;
+		bool inPattern=true;
+		int lastVal = 255;	
+		bool inWhite=false;
+		bool lastInWhite=false;
+	
+		int error_count=0;
+		
+
+		for(int i=0; i<size.width; i++){
+			Vec3b pixelData = cropped.at<Vec3b>(u,i);
+			int val = (pixelData[0] + pixelData[1] + pixelData[2])/3;
+			
+			//cout<<"i: "<<i<<" val: "<<val<<endl;
+			distance++;
+	
+			if((lastVal+val)/2>128)
+			{
+				inWhite=true;
+				//cout<<"inwhite: "<<i<<endl;
+			}	
+			else{
+				inWhite=false;
+				//cout<<"inBlack: "<<i<<endl;
+			}
+	
+			if(lastInWhite!=inWhite)
+			{	
+				int err = abs(lastDistance-distance);
+				if(err>1)
+					error_count+=err;		
+				amount_of_distances++;
+				lastDistance=distance;
+				distance=0;
+			}
+				
+			lastInWhite=inWhite;
+			lastVal = val;
+
+		}
+	
+		if(error_count<lowestError)
+		{
+			lowestError=error_count;
+			lowestErrorOffset = u;
+		}
 
 
-	namedWindow("Display Window", WINDOW_AUTOSIZE);
+		cout<<"Distance: "<<size.width/amount_of_distances<<endl;
+		cout<<"Error count: "<<error_count<<endl;
+	}
+
+	cout<<"Lowest error offset: "<<lowestErrorOffset<<endl;
+
+/*
+	for(int i=0; i<cropped.width; i++)
+	{
+		for(int u=0; u<cropped.height; u++)
+		{
+
+		}
+	}
+	
+/*
+
+
+
+
+
+
+
+/*
+	int result_cols = image.cols - pattern.cols+1;
+	int result_rows = image.rows - pattern.rows+1;
+
+	result.create(result_cols, result_rows, CV_32FC1);
+
+	matchTemplate(image, pattern, result, 0);
+	normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
+
+	double minVal;
+	double maxVal;
+	Point minLoc;
+	Point maxLoc;
+	Point matchLoc;
+
+	minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+
+	matchLoc = maxLoc;
+
+	cout<<"matchings:"<<endl;
+	cout<<"x: "<<matchLoc.x<<endl;
+	cout<<"y: "<<matchLoc.y<<endl;
+
+
+
+	counter++;
+//}
+
+
+	imwrite("output.png", pattern);		
+*/	
+
+/*	namedWindow("Display Window", WINDOW_AUTOSIZE);
 	imshow("Display Window", image);
 	
 	namedWindow("Image2", WINDOW_AUTOSIZE);
 	imshow("Image2", cropped);
+*/
 	waitKey(0);
 	return 0;
 	
