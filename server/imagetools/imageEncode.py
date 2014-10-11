77cabd37d8e5a2c8975ebd0fd13ef47b3da38721
IgorSusmelj <igor_7779@hotmail.com>
2014-10-11 03:05:52 -0700

    fixed image compression

--- server/imagetools/imageEncode.py
+++ server/imagetools/imageEncode.py
@@ -20,15 +20,18 @@ if __name__ == '__main__':
     parser.add_argument('tmpdir', help='the temporary directory that will be used')
 
     args = parser.parse_args()
-	
-    with open(args.filename, "rb") as file_in:
-        with gzip.open(args.filename+"_c", "wb") as file_out:
-            file_out.writelines(file_in)
 
+	
+    with open(args.file, "rb") as file_in:
+       with gzip.open(args.file+"_c", "wb") as file_out:
+          file_out.writelines(file_in)
 
 
     fname = tempfile.mkstemp(suffix='.png', dir=args.tmpdir)[-1]
-    im.encodeData(args.filename+"_c", readInString(args.file), fname)
+
+
+
+    im.encodeData(args.filename, readInString(args.file+"_c"), fname)
 
     print fname
 
