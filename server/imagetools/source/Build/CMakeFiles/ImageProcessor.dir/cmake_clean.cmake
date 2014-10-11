b072957165a827cc635c61f2375624444e2e366f
IgorSusmelj <igor_7779@hotmail.com>
2014-10-11 07:47:40 -0700

    added image cropping

---
+++ server/imagetools/source/Build/CMakeFiles/ImageProcessor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ImageProcessor.dir/pngToData.cpp.o"
+  "ImageProcessor.pdb"
+  "ImageProcessor"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ImageProcessor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
--- server/imagetools/source/pngToData.cpp
+++ server/imagetools/source/pngToData.cpp
@@ -2,6 +2,10 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
+#define BLACK_WHITE_THRESHOLD 200
+#define ROW_VALUE_THRESHOLD 50
+
+
 using namespace cv;
 using namespace std;
 
@@ -21,10 +25,165 @@ int main(int argc, char** argv)
 		cout<<"Could not open or find image"<<endl;
 		return -1;
 	}
+	
+
+
+	//Crop the image
+	Size size = image.size();
+
+	int xOffset=0;
+	int yOffset=0;
+	int uOffset=size.width;
+	int vOffset= size.height;		
+
+	
+
+	for(int i=0; i<size.height-1; i++)
+	{	
+
+		float rowValue=0.0f;
+		int rowSamples=1;
+
+
+		for(int u=0; u<size.width-1; u++)
+		{
+			Vec3b pixelValue = image.at<Vec3b>(i,u);
+			uchar b = pixelValue.val[0];
+			uchar g = pixelValue.val[1];
+			uchar r = pixelValue.val[2];
+			int p = 255-(r+g+b)/3.;
+			if(p>BLACK_WHITE_THRESHOLD)
+			{
+				rowValue+=p;
+				rowSamples++;
+			}	
+		}
+		if(rowValue/rowSamples>ROW_VALUE_THRESHOLD)
+		{
+			cout<<"Position X: "<<i<<" Value: "<<rowValue/rowSamples<<endl;
+			yOffset = i;
+			break;
+		}
+	}
+
+
+	for(int i=0; i<size.width-1; i++)
+	{	
+
+		float colValue=0.0f;
+		int colSamples=1;
+
+		for(int u=0; u<size.height-1; u++)
+		{
+			Vec3b pixelValue = image.at<Vec3b>(u,i);
+			uchar b = pixelValue.val[0];
+			uchar g = pixelValue.val[1];
+			uchar r = pixelValue.val[2];
+			int p = 255-(r+g+b)/3.;
+			if(p>BLACK_WHITE_THRESHOLD)
+			{
+				colValue+=p;
+				colSamples++;
+			}	
+		}
+		if(colValue/colSamples>ROW_VALUE_THRESHOLD)
+		{
+			cout<<"Position Y: "<<i<<" Value: "<<colValue/colSamples<<endl;
+			xOffset = i;
+			break;
+		}
+	}
+
+
+
+
+for(int i=size.width-1; i>0; i--)
+	{	
+
+		float colValue=0.0f;
+		int colSamples=1;
+
+		for(int u=size.height-1; u>0; u--)
+		{
+			Vec3b pixelValue = image.at<Vec3b>(u,i);
+			uchar b = pixelValue.val[0];
+			uchar g = pixelValue.val[1];
+			uchar r = pixelValue.val[2];
+			int p = 255-(r+g+b)/3.;
+			if(p>BLACK_WHITE_THRESHOLD)
+			{
+				colValue+=p;
+				colSamples++;
+			}	
+		}
+		if(colValue/colSamples>ROW_VALUE_THRESHOLD)
+		{
+			cout<<"Position U: "<<i<<" Value: "<<colValue/colSamples<<endl;
+			uOffset = i;
+			break;
+		}
+	}
+
+
+for(int i=size.height-1; i>0; i--)
+	{	
+
+		float colValue=0.0f;
+		int colSamples=1;
+
+		for(int u=size.width-1; u>0; u--)
+		{
+			Vec3b pixelValue = image.at<Vec3b>(i,u);
+			uchar b = pixelValue.val[0];
+			uchar g = pixelValue.val[1];
+			uchar r = pixelValue.val[2];
+			int p = 255-(r+g+b)/3.;
+			if(p>BLACK_WHITE_THRESHOLD)
+			{
+				colValue+=p;
+				colSamples++;
+			}	
+		}
+		if(colValue/colSamples>ROW_VALUE_THRESHOLD)
+		{
+			cout<<"Position V: "<<i<<" Value: "<<colValue/colSamples<<endl;
+			vOffset = i;
+			break;
+		}
+	}
+
+
+	//Do the actual cropping
+	Rect myROI(xOffset, yOffset, uOffset-xOffset, vOffset-yOffset);
+	Mat croppedRef(image, myROI);
+		
+	Mat cropped;
+	croppedRef.copyTo(cropped);
+
+	
+
+	//Detect pixel size
+	
+	size = cropped.size();
+		
+/*
+	for(int i=0; i<size.width; i++)
+	{
+		for(int u=0; u<size.height; u++)
+		{
+			
+		}
+	}*/
+
+
+	Mat pattern = cvCreateImage(cvSize(3,3),8,3);	
+
 
 	namedWindow("Display Window", WINDOW_AUTOSIZE);
 	imshow("Display Window", image);
-
+	
+	namedWindow("Image2", WINDOW_AUTOSIZE);
+	imshow("Image2", cropped);
 	waitKey(0);
 	return 0;
 	
