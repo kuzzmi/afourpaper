fa8b6ea70ec12308bcee3d95abf6ba780a902240
Igor Kuzmenko <kuzzmi@gmail.com>
2014-10-11 03:10:17 +0200

    Uploaded file handler added

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,27 +1,71 @@
 'use strict';
 
 angular.module('afourpaperApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
-    $scope.awesomeThings = [];
+    .controller('MainCtrl', function($scope, $http, socket, $upload) {
+        $scope.awesomeThings = [];
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-      socket.syncUpdates('thing', $scope.awesomeThings);
+        $http.get('/api/things').success(function(awesomeThings) {
+            $scope.awesomeThings = awesomeThings;
+            socket.syncUpdates('thing', $scope.awesomeThings);
+        });
+
+        $scope.addThing = function() {
+            if ($scope.newThing === '') {
+                return;
+            }
+            $http.post('/api/things', {
+                name: $scope.newThing
+            });
+            $scope.newThing = '';
+        };
+
+        $scope.deleteThing = function(thing) {
+            $http.delete('/api/things/' + thing._id);
+        };
+
+        $scope.$on('$destroy', function() {
+            socket.unsyncUpdates('thing');
+        });
+
+        $scope.myModelObj = null;
+
+        $scope.$watch('myModelObj', function(a, b) {
+            console.log(a, b);
+        });
+
+        $scope.onFileSelect = function($files) {
+            //$files: an array of files selected, each file has name, size, and type.
+            console.log($files);
+            for (var i = 0; i < $files.length; i++) {
+                var file = $files[i];
+                $scope.upload = $upload.upload({
+                    url: 'api/upload', //upload.php script, node.js route, or servlet url
+                    //method: 'POST' or 'PUT',
+                    //headers: {'header-key': 'header-value'},
+                    //withCredentials: true,
+                    data: {
+                        myObj: $scope.myModelObj
+                    },
+                    file: file, // or list of files ($files) for html5 only
+                    //fileName: 'doc.jpg' or ['1.jpg', '2.jpg', ...] // to modify the name of the file(s)
+                    // customize file formData name ('Content-Disposition'), server side file variable name. 
+                    //fileFormDataName: myFile, //or a list of names for multiple files (html5). Default is 'file' 
+                    // customize how data is added to formData. See #40#issuecomment-28612000 for sample code
+                    //formDataAppender: function(formData, key, val){}
+                }).progress(function(evt) {
+                    console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+                }).success(function(data, status, headers, config) {
+                    // file is uploaded successfully
+                    console.log(data);
+                });
+                //.error(...)
+                //.then(success, error, progress); 
+                // access or attach event listeners to the underlying XMLHttpRequest.
+                //.xhr(function(xhr){xhr.upload.addEventListener(...)})
+            }
+            /* alternative way of uploading, send the file binary with the file's content-type.
+             Could be used to upload files to CouchDB, imgur, etc... html5 FileReader is needed. 
+             It could also be used to monitor the progress of a normal http post/put request with large data*/
+            // $scope.upload = $upload.http({...})  see 88#issuecomment-31366487 for sample code.
+        };
     });
-
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
-
-    $scope.$on('$destroy', function () {
-      socket.unsyncUpdates('thing');
-    });
-  });
--- server/routes.js
+++ server/routes.js
@@ -8,19 +8,31 @@ var errors = require('./components/errors');
 
 module.exports = function(app) {
 
-  // Insert routes below
-  app.use('/api/things', require('./api/thing'));
-  app.use('/api/users', require('./api/user'));
+    // Insert routes below
+    app.use('/api/things', require('./api/thing'));
+    app.use('/api/users', require('./api/user'));
 
-  app.use('/auth', require('./auth'));
-  
-  // All undefined asset or api routes should return a 404
-  app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-   .get(errors[404]);
+    var multer = require('multer');
+    app.use(multer({
+        dest: './uploads/'
+    }));
 
-  // All other routes should redirect to the index.html
-  app.route('/*')
-    .get(function(req, res) {
-      res.sendfile(app.get('appPath') + '/index.html');
+    app.post('/api/upload', function(req, res, next) {
+        console.log(req.headers, req.files);
+        res.json({
+            status: 'ok'
+        });
     });
+
+    app.use('/auth', require('./auth'));
+
+    // All undefined asset or api routes should return a 404
+    app.route('/:url(api|auth|components|app|bower_components|assets)/*')
+        .get(errors[404]);
+
+    // All other routes should redirect to the index.html
+    app.route('/*')
+        .get(function(req, res) {
+            res.sendfile(app.get('appPath') + '/index.html');
+        });
 };
